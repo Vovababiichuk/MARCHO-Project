5f09611f3befef524f5714f424ff4e5c99b7f771
Vovababiichuk <babihuk888@gmail.com>
2023-03-18 02:36:58 +0200

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function () {
-  // $('body').hide();
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000
 });
 
-     
+});    
